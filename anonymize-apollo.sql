a5349a7dedd87423a069453ea9bf2d67176a6a8c
Nik Stankovic <nik@enfsolar.com>
2023-05-25 18:42:41 +0800

    fix sql statements

--- anonymize-apollo.sql
+++ anonymize-apollo.sql
@@ -1,32 +1,38 @@
 --
 -- p_inquire
 --
-UPDATE entity_company SET `phone` = "+1 212 5551212";
-UPDATE entity_company SET `fax` = "+1 212 5551212";
-UPDATE entity_company SET `email` = "anon@enfsolar.com";
-UPDATE entity_company SET `email_panel` = "anon@enfsolar.com";
-UPDATE entity_company SET `email_inverter` = "anon@enfsolar.com";
-UPDATE entity_company SET `email_mounting_system` = "anon@enfsolar.com";
-UPDATE entity_company SET `email_cell` = "anon@enfsolar.com";
-UPDATE entity_company SET `email_eva` = "anon@enfsolar.com";
-UPDATE entity_company SET `email_backsheet` = "anon@enfsolar.com";
-UPDATE entity_company SET `social_weibo` = NULL;
-UPDATE entity_company SET `social_linkedin` = NULL;
-UPDATE entity_company SET `social_facebook` = NULL;
-UPDATE entity_company SET `social_twitter` = NULL;
-UPDATE entity_company SET `address` = "1 Park St, New York, NY";
-UPDATE entity_company SET `website` = "https://www.enfsolar.com";
-UPDATE entity_company SET `x` = "36.0601649";
-UPDATE entity_company SET `y` = "-80.2277113";
-UPDATE entity_company SET `linkedin` = "-1";
-UPDATE entity_company SET `url_name` = "url-name";
+UPDATE entity_company SET
+  `phone` = "+1 212 5551212",
+  `fax` = "+1 212 5551212",
+  `email` = "anon@enfsolar.com",
+  `email_panel` = "anon@enfsolar.com",
+  `email_inverter` = "anon@enfsolar.com",
+  `email_mounting_system` = "anon@enfsolar.com",
+  `email_cell` = "anon@enfsolar.com",
+  `email_eva` = "anon@enfsolar.com",
+  `email_backsheet` = "anon@enfsolar.com",
+  `social_weibo` = NULL,
+  `social_linkedin` = NULL,
+  `social_facebook` = NULL,
+  `social_twitter` = NULL,
+  `address` = "1 Park St, New York, NY",
+  `website` = "https://www.enfsolar.com",
+  `x` = "36.0601649",
+  `y` = "-80.2277113",
+  `linkedin` = "-1",
+  `url_name` = "url-name";
+
 
 --
 -- p_inquire
 --
-UPDATE p_inquire SET `phone` = "+1 212 5551212";
-UPDATE p_inquire SET `email` = "anon@enfsolar.com";
-UPDATE p_inquire SET `skype` = NULL;
-UPDATE p_inquire SET `qq` = NULL;
-UPDATE p_inquire SET `comments` = "enquiry text";
-UPDATE p_inquire SET `comments_checked` = "enquiry text";
+UPDATE p_inquire SET
+    `name` = 'Bob Jonhson',
+    `company_name` = 'FBI Solar Inc.',
+    `phone` = '+1 212 5551212',
+    `email` = 'anon@enfsolar.com',
+    `skype` = NULL,
+    `qq` = NULL,
+    `comments` = 'enquiry text',
+    `comments_checked` = 'enquiry text';
+
